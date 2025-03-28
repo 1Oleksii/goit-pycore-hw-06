a66c1e9e8e303745169c74dc579aab7374c1ff71
Алексей <aleksej@MacBook-Pro-Aleksej.local>
2025-03-28 16:46:20 +0200

    Remove unnecessary files from the repository

--- homework.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-url = "https://uk.wikipedia.org/wiki/Населення_України#Народжуваність"
-tables = pd.read_html(url)
-
-for i, table in enumerate(tables):
-    print(f"Таблиця {i}:")
-    print(table.head(), "\n")
